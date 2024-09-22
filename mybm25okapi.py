d4dc43bc52f75d46067309422f903f689d53fe84
András Jankovics <jankovicsandras@gmail.com>
2024-09-22 21:26:26 +0200

    Update mybm25okapi.py

--- mybm25okapi.py
+++ mybm25okapi.py
@@ -1,18 +1,12 @@
 """
-This is a refactored variant of rank_bm25 Okapi.
+This is a refactored variant of rank_bm25 Okapi. https://github.com/dorianbrown/rank_bm25
+This is not required for plpgsql_bm25, just here for comparison / testing.
 
 Usage:
-  - corpus and query must be tokenized already, e.g. corpus = [ ['one','two','three'], ['bla','two','two'] ]  ; query = [ 'Is', 'this', 'a', 'question?' ]
+  - corpus and query must be tokenized already, e.g. corpus = [ ['one','two','three'], ['bla','two','two'] ]  ; query = [ 'Is', 'this', 'a', 'question?' ] ; see also mytokenize()
   - __init__(corpus) will initialize the bm25Okapi components, where self.wsmap is the most important
   - No update is possible, so if the documents change in the corpus, then __init__(corpus) must be called again (recreating all the components).
   - search with topk() or get_scores()
-
-Usage with Postgres:
-  - corpus and query must be tokenized already, e.g. corpus = [ ['one','two','three'], ['bla','two','two'] ]  ; query = [ 'Is', 'this', 'a', 'question?' ]
-  - __init__(corpus) will initialize the bm25Okapi components, where self.wsmap is the most important
-  - No update is possible, so if the documents change in the corpus, then __init__(corpus) must be called again (recreating all the components).
-  - call exportwsmap() after init, then import wsmap into a Postgres table: COPY tablename_bm25wsmap FROM '/path-to/tablename_bm25wsmap.csv' DELIMITER ';' CSV HEADER;
-  - search in Postgres by calling the plpgsql functions: SELECT bm25topk.id, bm25topk.score, bm25topk.doc FROM bm25topk(tablename, tablename_bm25wsmap, query, 10);
 """
 import math
 
@@ -20,13 +14,14 @@ import math
 class mybm25okapi:
   def __init__(self, corpus):
     # constants
+    self.debugmode = False
     self.k1 = 1.5
     self.b = 0.75
     self.epsilon = 0.25
 
     self.corpus_len = len(corpus)
     self.avg_doc_len = 0
-    self.doc_freqs = []
+    self.word_freqs = []
     self.idf = {}
     self.doc_lens = []
     word_docs_count = {}  # word -> number of documents with word
@@ -43,7 +38,7 @@ class mybm25okapi:
         if word not in frequencies:
           frequencies[word] = 0
         frequencies[word] += 1
-      self.doc_freqs.append(frequencies)
+      self.word_freqs.append(frequencies)
 
       # number of documents with word count
       for word, freq in frequencies.items():
@@ -54,6 +49,12 @@ class mybm25okapi:
 
     # average document length
     self.avg_doc_len = total_word_count / self.corpus_len
+
+    if self.debugmode : print('self.corpus_len',self.corpus_len,'\nself.doc_lens',self.doc_lens,'\ntotal_word_count',total_word_count,'\nself.word_freqs',self.word_freqs,'\nself.avg_doc_len',self.avg_doc_len,'\nword_docs_count',word_docs_count)
+
+    # precalc "half of divisor" + self.k1 * (1 - self.b + self.b * doc_lens / self.avg_doc_len)
+    self.hds = [ self.k1 * ( 1-self.b + self.b*doc_len/self.avg_doc_len) for doc_len in self.doc_lens ]
+    if self.debugmode : print('self.hds',self.hds)
 
     """
     Calculates frequencies of terms in documents and in corpus.
@@ -70,22 +71,28 @@ class mybm25okapi:
       idf_sum += idf
       if idf < 0:
         negative_idfs.append(word)
+      if self.debugmode : print('word',word,'self.idf[word]',self.idf[word])
     self.average_idf = idf_sum / len(self.idf)
+    if self.debugmode : print('idf_sum',idf_sum,'len(self.idf)',len(self.idf),'self.average_idf',self.average_idf)
     # assign epsilon
     eps = self.epsilon * self.average_idf
+    if self.debugmode : print('eps',eps)
     for word in negative_idfs:
       self.idf[word] = eps
+      if self.debugmode : print('word',word,'got eps',eps)
+    if self.debugmode : print('self.idf',self.idf)
 
-    # precalc "half of divisor" + self.k1 * (1 - self.b + self.b * doc_lens / self.avg_doc_len)
-    self.hds = [ self.k1 * ( 1-self.b + self.b*doc_len/self.avg_doc_len) for doc_len in self.doc_lens ]
 
     # words * documents score map
     self.wsmap = {}
     for word in self.idf :
       self.wsmap[word] = [0] * self.corpus_len
-      word_freqs = [ (doc.get(word) or 0) for doc in self.doc_freqs ]
+      word_freqs = [ (word_freq.get(word) or 0) for word_freq in self.word_freqs ]
+      thiswordidf = (self.idf.get(word) or 0)
+      if self.debugmode : print('word in self.idf',word,'thiswordidf',thiswordidf,'word_freqs',word_freqs)
       for i in range(0,self.corpus_len) :
-        self.wsmap[word][i] += (self.idf.get(word) or 0) * ( word_freqs[i] * (self.k1 + 1) / ( word_freqs[i] + self.hds[i] ) )
+        self.wsmap[word][i] = thiswordidf * ( word_freqs[i] * (self.k1 + 1) / ( word_freqs[i] + self.hds[i] ) ) # += replaced with =
+    if self.debugmode : print('self.wsmap',self.wsmap)
 
 
   # get a list of scores for every document
@@ -122,8 +129,8 @@ class mybm25okapi:
 
 # tokenization function
 def mytokenize(s) :
-  ltrimchars = ['(','[','{','<']
-  rtrimchars = ['.', '?', '!', ',', ':', ';', ')', ']', '}', '>']
+  ltrimchars = ['(','[','{','<','\'','"']
+  rtrimchars = ['.', '?', '!', ',', ':', ';', ')', ']', '}', '>','\'','"']
   if type(s) != str : return []
   wl = s.lower().split()
   for i,w in enumerate(wl) :
