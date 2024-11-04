692dfeacb00b3bd14d3fc05fa760a7bbfc12620d
András Jankovics <jankovicsandras@gmail.com>
2024-11-04 08:22:39 +0100

    Update plpgsql_bm25.sql

--- plpgsql_bm25.sql
+++ plpgsql_bm25.sql
@@ -1,5 +1,5 @@
 
-/* 
+/*
 
   plpgsql_bm25.sql
   BM25 Okapi search implemented in PL/pgSQL
@@ -9,7 +9,7 @@
   SELECT bm25createindex( tablename, columnname );  /* tablename and columnname are TEXT types */
   SELECT * FROM bm25topk( tablename, columnname, question, k ); /* question is TEXT, k is INTEGER */
 
-  Please note: bm25createindex will (re)create new tables <tablename>_bm25i_docs and <tablename>_bm25i_words, 
+  Please note: bm25createindex will (re)create new tables <tablename>_bm25i_docs and <tablename>_bm25i_words,
   document results will come from <tablename>_bm25i_docs, not the original <tablename>. The algorithm can't
   track changes, so if documents change in the original <tablename>, then bm25createindex() must be called again.
 
@@ -84,8 +84,41 @@ AS $$
 DECLARE
   res DOUBLE PRECISION[];
 BEGIN
-  /* self.wsmap[word][i] = thiswordidf * ( word_freqs[i] * (self.k1 + 1) / ( word_freqs[i] + self.hds[i] ) ) # += replaced with = */
-  EXECUTE FORMAT( 'SELECT ARRAY_AGG( %s * COALESCE(word_freqs->>%s,%s)::INTEGER * %s / ( COALESCE(word_freqs->>%s,%s)::INTEGER + hds ) ORDER BY id) FROM %s;', thisidf, quote_literal(word), quote_literal(0), (thisk1+1), quote_literal(word), quote_literal(0), docstname ) INTO res;
+  /* # self.wsmap[word][i] = thiswordidf * ( word_freqs[i] * (self.k1 + 1) / ( word_freqs[i] + self.hds[i] ) ) */
+  EXECUTE FORMAT( 'SELECT ARRAY_AGG( %s * COALESCE(word_freqs->>%s,%s)::INTEGER * %s / ( COALESCE(word_freqs->>%s,%s)::INTEGER + ( %s * hds ) ) ORDER BY id) FROM %s;',
+    thisidf, quote_literal(word), quote_literal(0), (thisk1+1), quote_literal(word), quote_literal(0), thisk1, docstname ) INTO res;
+  RETURN res;
+END;
+$$;
+
+
+/* get_wsmapobjl() */
+DROP FUNCTION IF EXISTS get_wsmapobjl;
+CREATE OR REPLACE FUNCTION get_wsmapobjl( docstname TEXT, word TEXT, thisidf DOUBLE PRECISION, thisk1 DOUBLE PRECISION, thisdelta DOUBLE PRECISION ) RETURNS DOUBLE PRECISION[]
+LANGUAGE plpgsql
+AS $$
+DECLARE
+  res DOUBLE PRECISION[];
+BEGIN
+  /* # self.wsmap[word][i] = self.idf[word] * twf * (self.k1 + 1) * ( twf/self.hds[di] + self.delta) / (self.k1 + twf/self.hds[di] + self.delta) */
+  EXECUTE FORMAT( 'SELECT ARRAY_AGG( %s * COALESCE(word_freqs->>%s,%s)::INTEGER * %s * ( COALESCE(word_freqs->>%s,%s)::INTEGER / hds + %s ) / ( %s + COALESCE(word_freqs->>%s,%s)::INTEGER / hds + %s ) ORDER BY id) FROM %s;',
+    thisidf, quote_literal(word), quote_literal(0), (thisk1+1), quote_literal(word), quote_literal(0), thisdelta, thisk1, quote_literal(word), quote_literal(0), thisdelta, docstname ) INTO res;
+  RETURN res;
+END;
+$$;
+
+
+/* get_wsmapobjplus() */
+DROP FUNCTION IF EXISTS get_wsmapobjplus;
+CREATE OR REPLACE FUNCTION get_wsmapobjplus( docstname TEXT, word TEXT, thisidf DOUBLE PRECISION, thisk1 DOUBLE PRECISION, thisdelta DOUBLE PRECISION ) RETURNS DOUBLE PRECISION[]
+LANGUAGE plpgsql
+AS $$
+DECLARE
+  res DOUBLE PRECISION[];
+BEGIN
+  /* # self.wsmap[word][i] = self.idf[word] * (self.delta + ( twf * (self.k1 + 1) / ( twf + self.k1 * self.hds[di] ) )) */
+  EXECUTE FORMAT( 'SELECT ARRAY_AGG( %s * ( %s + ( COALESCE(word_freqs->>%s,%s)::INTEGER * %s / ( COALESCE(word_freqs->>%s,%s)::INTEGER + %s * hds ) ) ) ORDER BY id) FROM %s;',
+    thisidf, thisdelta, quote_literal(word), quote_literal(0), (thisk1+1), quote_literal(word), quote_literal(0), thisk1, docstname ) INTO res;
   RETURN res;
 END;
 $$;
@@ -93,15 +126,16 @@ $$;
 
 /* bm25createindex() */
 DROP FUNCTION IF EXISTS bm25createindex;
-CREATE OR REPLACE FUNCTION bm25createindex(tablename TEXT, columnname TEXT) RETURNS VOID
+CREATE OR REPLACE FUNCTION bm25createindex(tablename TEXT, columnname TEXT, algo TEXT DEFAULT '') RETURNS VOID
 LANGUAGE plpgsql
 AS $$
 DECLARE
-  docstname TEXT := tablename || '_' ||  columnname || '_bm25i_docs';
-  wordstname TEXT := tablename || '_' ||  columnname || '_bm25i_words';
+  docstname TEXT := tablename || '_' ||  columnname || '_bm25i_docs' || algo;
+  wordstname TEXT := tablename || '_' ||  columnname || '_bm25i_words' || algo;
   param_k1 DOUBLE PRECISION := 1.5;
   param_b DOUBLE PRECISION := 0.75;
   param_epsilon DOUBLE PRECISION := 0.25;
+  param_delta DOUBLE PRECISION := 1.0;
   corpus_len INTEGER;
   vocab_len INTEGER;
   total_word_count INTEGER;
@@ -119,6 +153,21 @@ BEGIN
   INSERT INTO bm25_params_debug(paramname,value) VALUES('param_b',param_b);
   INSERT INTO bm25_params_debug(paramname,value) VALUES('param_epsilon',param_epsilon);
   */
+
+  /* BM25L and BM25Plus parameters */
+  IF algo = 'l' THEN
+    param_k1 := 1.5;
+    param_b := 0.75;
+    param_delta := 0.5;
+    param_epsilon := 0.25;
+  END IF;
+
+  IF algo = 'plus' THEN
+    param_k1 := 1.5;
+    param_b := 0.75;
+    param_delta := 1;
+    param_epsilon := 0.25;
+  END IF;
 
   /* create docs table */
   EXECUTE FORMAT( 'DROP TABLE IF EXISTS %s;', docstname );
@@ -154,18 +203,29 @@ BEGIN
   /*INSERT INTO bm25_params_debug(paramname,value) VALUES('corpus_len',corpus_len);
   INSERT INTO bm25_params_debug(paramname,value) VALUES('avg_doc_len',avg_doc_len);*/
 
-  /*  # precalc "half of divisor" + self.k1 * (1 - self.b + self.b * doc_lens / self.avg_doc_len)  */
+  /*  # precalc "half of divisor" (1 - self.b + self.b * doc_lens / self.avg_doc_len)  */
   EXECUTE FORMAT( 'ALTER TABLE %s ADD COLUMN hds DOUBLE PRECISION;', docstname );
-  EXECUTE FORMAT( 'UPDATE %s SET hds = %s * ( 1.0::DOUBLE PRECISION - %s + %s * doc_lens / %s ) ;', docstname, param_k1, param_b, param_b, avg_doc_len );
+  EXECUTE FORMAT( 'UPDATE %s SET hds = ( 1.0::DOUBLE PRECISION - %s + %s * doc_lens / %s ) ;', docstname,  param_b, param_b, avg_doc_len );
 
+  /* BM25Okapi IDF | idf = math.log(self.corpus_len - freq + 0.5) - math.log(freq + 0.5) ; self.idf[word] = idf ; idf_sum += idf */
+  IF algo = '' THEN
+    EXECUTE FORMAT( 'UPDATE %s SET idf = LN( %s - word_docs_count::DOUBLE PRECISION + 0.5::DOUBLE PRECISION) - LN( word_docs_count::DOUBLE PRECISION + 0.5::DOUBLE PRECISION);', wordstname, corpus_len::DOUBLE PRECISION );
+    EXECUTE FORMAT( 'SELECT SUM(idf) FROM %s;', wordstname ) INTO idf_sum;
+    EXECUTE FORMAT( 'SELECT COUNT(word) FROM %s;', wordstname ) INTO vocab_len;
+    average_idf = idf_sum / vocab_len::DOUBLE PRECISION;
+    param_eps = param_epsilon * average_idf;
+    EXECUTE FORMAT( 'UPDATE %s SET idf = %s WHERE idf < 0;', wordstname, param_eps );
+  END IF;
 
-  /* idf = math.log(self.corpus_len - freq + 0.5) - math.log(freq + 0.5) ; self.idf[word] = idf ; idf_sum += idf */
-  EXECUTE FORMAT( 'UPDATE %s SET idf = LN( %s - word_docs_count::DOUBLE PRECISION + 0.5::DOUBLE PRECISION) - LN( word_docs_count::DOUBLE PRECISION + 0.5::DOUBLE PRECISION);', wordstname, corpus_len::DOUBLE PRECISION );
-  EXECUTE FORMAT( 'SELECT SUM(idf) FROM %s;', wordstname ) INTO idf_sum;
-  EXECUTE FORMAT( 'SELECT COUNT(word) FROM %s;', wordstname ) INTO vocab_len;
-  average_idf = idf_sum / vocab_len::DOUBLE PRECISION;
-  param_eps = param_epsilon * average_idf;
-  EXECUTE FORMAT( 'UPDATE %s SET idf = %s WHERE idf < 0;', wordstname, param_eps );
+  /* BM25L IDF */
+  IF algo = 'l' THEN
+    EXECUTE FORMAT( 'UPDATE %s SET idf = LN( %s + 1.0::DOUBLE PRECISION ) - LN( word_docs_count::DOUBLE PRECISION + 0.5::DOUBLE PRECISION);', wordstname, corpus_len::DOUBLE PRECISION );
+  END IF;
+
+  /* BM25Plus IDF */
+  IF algo = 'plus' THEN
+    EXECUTE FORMAT( 'UPDATE %s SET idf = LN( %s + 1.0::DOUBLE PRECISION ) - LN( word_docs_count::DOUBLE PRECISION );', wordstname, corpus_len::DOUBLE PRECISION );
+  END IF;
 
   /* these debug statements are not required */
   /*INSERT INTO bm25_params_debug(paramname,value) VALUES('idf_sum',idf_sum);
@@ -175,7 +235,17 @@ BEGIN
 
   /*  words * documents score map  */
   EXECUTE FORMAT( 'ALTER TABLE %s ADD COLUMN wsmap DOUBLE PRECISION[];', wordstname );
-  EXECUTE FORMAT( 'UPDATE %s SET wsmap = get_wsmapobj( %s, word, idf, %s );', wordstname, quote_literal(docstname), param_k1 );
+  IF algo = '' THEN
+    EXECUTE FORMAT( 'UPDATE %s SET wsmap = get_wsmapobj( %s, word, idf, %s );', wordstname, quote_literal(docstname), param_k1 );
+  END IF;
+
+  IF algo = 'l' THEN
+    EXECUTE FORMAT( 'UPDATE %s SET wsmap = get_wsmapobjl( %s, word, idf, %s, %s );', wordstname, quote_literal(docstname), param_k1, param_delta );
+  END IF;
+
+  IF algo = 'plus' THEN
+    EXECUTE FORMAT( 'UPDATE %s SET wsmap = get_wsmapobjplus( %s, word, idf, %s, %s );', wordstname, quote_literal(docstname), param_k1, param_delta );
+  END IF;
 
 END;
 $$;
@@ -236,14 +306,13 @@ $$;
 
 /* bm25topk(): returns the index, score and document sorted and limited |  TABLE(id INT, id2 BIGINT, score double precision, doc TEXT) */
 DROP FUNCTION IF EXISTS bm25topk;
-CREATE OR REPLACE FUNCTION bm25topk(tablename TEXT, columnname TEXT, tokenizedquery TEXT, k INT) RETURNS TABLE(id INTEGER, score double precision, doc TEXT)
+CREATE OR REPLACE FUNCTION bm25topk(tablename TEXT, columnname TEXT, tokenizedquery TEXT, k INT, algo TEXT DEFAULT '') RETURNS TABLE(id INTEGER, score double precision, doc TEXT)
 LANGUAGE plpgsql
 AS $$
 DECLARE
-  docstname TEXT := tablename || '_' ||  columnname || '_bm25i_docs';
-  wordstname TEXT := tablename || '_' ||  columnname || '_bm25i_words';
+  docstname TEXT := tablename || '_' ||  columnname || '_bm25i_docs' || algo;
+  wordstname TEXT := tablename || '_' ||  columnname || '_bm25i_words' || algo;
 BEGIN
   RETURN QUERY EXECUTE FORMAT( 'SELECT t1.id, t2.score, t1.%s AS doc FROM (SELECT id, doc AS %s FROM %s) t1 INNER JOIN ( SELECT id, score FROM bm25isc(%s,%s) ) t2 ON ( t1.id = t2.id ) ORDER BY t2.score DESC LIMIT %s;', columnname, columnname, docstname, quote_literal(wordstname), quote_literal(tokenizedquery), k );
 END;
 $$;
-
